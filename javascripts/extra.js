f7a78c618261f35415a6755e9d1b2a8e053ad59d
agarciaripalda <agarciaripalda@alumnos.alborfp.com>
2026-01-16 12:18:02 +0100

    Deployed 4a5d81e with MkDocs version: 1.6.1

---
+++ javascripts/extra.js
@@ -0,0 +1,7 @@
+/* Extra JavaScript for WoW Test Manager Documentation */
+
+document.addEventListener('DOMContentLoaded', function () {
+    console.log('WoW Test Manager Documentation loaded');
+
+    // Add custom functionality here if needed
+});
